ee0919fb8a704913a6d897d253eabb3335d7102b
adelorosia <adel_orosia@yahoo.com>
2023-06-15 21:06:37 +0200

    edited

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import {
+  ActiveContextProvider,
+  SearchContextProvider,
+} from "./context/context.tsx";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <ActiveContextProvider>
+   
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+   
+  </ActiveContextProvider>
+);
