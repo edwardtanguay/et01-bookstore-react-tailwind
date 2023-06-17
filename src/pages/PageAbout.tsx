fd9dcff8042ff2e02128ce9018bc5f423fa45241
Edward Tanguay (windows) <edward@tanguay.info>
2023-06-17 23:11:55 +0200

    about page with text

--- src/pages/PageAbout.tsx
+++ src/pages/PageAbout.tsx
@@ -1,7 +1,26 @@
 export const PageAbout = () => {
-  return (
-    <div>
-      about
-    </div>
-  )
-}
+	return (
+		<div>
+			<p className="py-6">
+				This site was refactored from{' '}
+				<a
+					className="text-blue-600 hover:underline"
+					href="https://github.com/edwardtanguay/et01-bookstore-react-tailwind-unrefactored"
+				>
+					this site
+				</a>{' '}
+				for learning purposes.
+			</p>
+			<p>
+				Thanks to{' '}
+				<a
+					className="text-blue-600 hover:underline"
+					href="https://github.com/adelorosia"
+				>
+					Adel
+				</a>{' '}
+				for the original site created with Tailwind.
+			</p>
+		</div>
+	);
+};
